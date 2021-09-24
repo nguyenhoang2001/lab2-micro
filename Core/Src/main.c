2c2fa64d245fdac48a28ed149254069865583ffa
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-09-24 21:00:53 +0700

    testing

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,8 +94,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  HAL_GPIO_TogglePin(nothing_GPIO_Port, nothing_Pin);
-	  HAL_Delay(1000);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
