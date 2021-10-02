1dd1d62dc1816b85f0817d56f53f4d8ba76d83f8
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-10-02 14:47:09 +0700

    configure ex3

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,8 +58,32 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define nothing_Pin GPIO_PIN_1
-#define nothing_GPIO_Port GPIOA
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
+#define LED_Pin GPIO_PIN_5
+#define LED_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
+#define LED1_A_Pin GPIO_PIN_0
+#define LED1_A_GPIO_Port GPIOB
+#define LED1_B_Pin GPIO_PIN_1
+#define LED1_B_GPIO_Port GPIOB
+#define LED1_C_Pin GPIO_PIN_2
+#define LED1_C_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
+#define LED1_D_Pin GPIO_PIN_3
+#define LED1_D_GPIO_Port GPIOB
+#define LED1_E_Pin GPIO_PIN_4
+#define LED1_E_GPIO_Port GPIOB
+#define LED1_F_Pin GPIO_PIN_5
+#define LED1_F_GPIO_Port GPIOB
+#define LED1_G_Pin GPIO_PIN_6
+#define LED1_G_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
