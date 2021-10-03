55077ec992d974c602f57ac54b238f7f60562dfa
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-10-03 13:31:40 +0700

    finish ex4

--- Core/Src/function_configuration.c
+++ Core/Src/function_configuration.c
@@ -13,40 +13,13 @@ static uint8_t sevenSegmentLEDConversion[NUMBER_OF_DECIMAL_DIGITS] = {0x01, 0x4f
 
 void display7SEG1(int num) {
 	uint8_t the_number = sevenSegmentLEDConversion[num];
-	if(the_number & 0x01) {
-		HAL_GPIO_WritePin(LED1_G_GPIO_Port, LED1_G_Pin, 1);
-	} else
-		HAL_GPIO_WritePin(LED1_G_GPIO_Port, LED1_G_Pin, 0);
-
-	if(the_number & 0x02) {
-			HAL_GPIO_WritePin(LED1_F_GPIO_Port, LED1_F_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_F_GPIO_Port, LED1_F_Pin, 0);
-
-	if(the_number & 0x04) {
-			HAL_GPIO_WritePin(LED1_E_GPIO_Port, LED1_E_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_E_GPIO_Port, LED1_E_Pin, 0);
-
-	if(the_number & 0x08) {
-			HAL_GPIO_WritePin(LED1_D_GPIO_Port, LED1_D_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_D_GPIO_Port, LED1_D_Pin, 0);
-
-	if(the_number & 0x10) {
-			HAL_GPIO_WritePin(LED1_C_GPIO_Port, LED1_C_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_C_GPIO_Port, LED1_C_Pin, 0);
-
-	if(the_number & 0x20) {
-			HAL_GPIO_WritePin(LED1_B_GPIO_Port, LED1_B_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_B_GPIO_Port, LED1_B_Pin, 0);
-
-	if(the_number & 0x40) {
-			HAL_GPIO_WritePin(LED1_A_GPIO_Port, LED1_A_Pin, 1);
-		} else
-			HAL_GPIO_WritePin(LED1_A_GPIO_Port, LED1_A_Pin, 0);
+	HAL_GPIO_WritePin(LED1_G_GPIO_Port, LED1_G_Pin, the_number & 0x01);
+	HAL_GPIO_WritePin(LED1_F_GPIO_Port, LED1_F_Pin, the_number & 0x02);
+	HAL_GPIO_WritePin(LED1_E_GPIO_Port, LED1_E_Pin, the_number & 0x04);
+	HAL_GPIO_WritePin(LED1_D_GPIO_Port, LED1_D_Pin, the_number & 0x08);
+	HAL_GPIO_WritePin(LED1_C_GPIO_Port, LED1_C_Pin, the_number & 0x10);
+	HAL_GPIO_WritePin(LED1_B_GPIO_Port, LED1_B_Pin, the_number & 0x20);
+	HAL_GPIO_WritePin(LED1_A_GPIO_Port, LED1_A_Pin, the_number & 0x40);
 }
 
 const int MAX_LED = 4;
